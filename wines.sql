0b121b5d9a1bf91aee3d79686569d4ef43e4027e
emilywashburn <emilywashburn1@gmail.com>
2023-08-03 20:59:35 -0400

    added sql

---
+++ wines.sql
@@ -0,0 +1,16 @@
+create table wines (
+wine_id INT,
+fixed_acidity FLOAT,
+volatile_acidity FLOAT,
+citric_acid FLOAT,
+residual_sugar FLOAT,
+chlorides FLOAT,
+free_sulfur_dioxide FLOAT,
+total_sulfur_dioxide FLOAT,
+density float,
+pH float,
+sulphates float,
+alcohol	float,
+quality	int,
+type varchar
+);
